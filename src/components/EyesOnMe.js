32a529053c7379cfdbaa78034c5477ab0f1d0fc8
Monalisa Onyango <+>
2024-08-01 20:07:37 +0300

    event handling

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
 // Code EyesOnMe Component Here
+
+function EyesOnMe(){
+
+    function focusOn(){
+        console.log("Good!")
+    }
+
+    function blurOn(){
+        console.log("Hey! Eyes on me!")
+    }
+
+    return(
+        <div>
+            <button onFocus={focusOn}  onBlur={blurOn}>Eyes on me </button>
+
+        </div>
+    )
+}
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,21 @@
 // Code Keypad Component Here
 
 function Keypad (){
+
+    function handleKeypad(event){
+        console.log('Entering password...')
+        // console.log(`${event.target.value}`)
+
+    }
     return (
-        <div></div>
+        <div>
+            <input type="password"
+                   onChange={handleKeypad}
+                   placeholder="Enter password"
+
+            />
+
+        </div>
     )
 }
 
